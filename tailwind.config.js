f20285a240aef0ef32a476cc621543f13b8ec6dd
simsam897 <simma013s@gmail.com>
2025-04-17 12:03:10 +0530

    fixed issues in tailwind config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,17 @@
+// /** @type {import('tailwindcss').Config} */
+// module.exports = {
+//   content: ["./public/razorpay.html"],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// };
+
+
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./public/razorpay.html"],
+  content: ["./public/**/*.html", "./src/**/*.{html,js}"],
   theme: {
     extend: {},
   },
